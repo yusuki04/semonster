a2943ef02a53fcf1c92fc6a2c9aadbedd48819c6
yusuki04 <yusuriku@icloud.com>
2026-07-09 15:37:41 +0900

    設定ファイルをまとめてコミット

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMONSTER GAME");
+    System.out.println("Show anew monster!");
   }
 }
